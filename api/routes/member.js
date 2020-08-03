07a0499aaa352f6a4b84eaeb52db2e2090694ade
ketarWorapol <48780928+ketarWorapol@users.noreply.github.com>
2020-08-03 09:11:07 +0700

    push to server

--- api/routes/member.js
+++ api/routes/member.js
@@ -6,21 +6,21 @@ const mysql = require("mysql");
 
 // ประกาศตัวแปร แบบ Global
 var mysql_connection = mysql.createConnection({
-  host: "ns117.hostinglotus.net",
-  user: "dntcom_dao",
-  password: "123456@q!",
-  database: "dntcom_deejung",
-  multipleStatements: true,
+    host: "ns117.hostinglotus.net",
+    user: "dntcom_dao",
+    password: "123456@q!",
+    database: "dntcom_deejung",
+    multipleStatements: true,
 });
 
 // C => Created || SIGNUP
-router.post('/', (req, res, next)=>{
+router.post('/', (req, res, next) => {
     let model = req.body;
     model.created = new Date().getTime();
     model.updated = new Date().getTime();
 
-    bcrypt.hash(model.password, 10, (err,hash)=>{
-        if(err){
+    bcrypt.hash(model.password, 10, (err, hash) => {
+        if (err) {
             return res.status(500).json({
                 error: err.message
             });
@@ -28,17 +28,17 @@ router.post('/', (req, res, next)=>{
 
         var sql = "INSERT INTO member (name, username, password, role, status, company, created, updated) VALUES (";
         sql += "'" + model.name + "', '" + model.username + "', '" + hash + "', " + model.role + ", " + model.status + ", " + model.company
-        sql += ", " +  model.created  + ", " + model.updated +")";
+        sql += ", " + model.created + ", " + model.updated + ")";
         model.password = hash;
-        mysql_connection.query(sql, (err, rows, fields)=>{
+        mysql_connection.query(sql, (err, rows, fields) => {
             model.created = new Date(model.created);
             model.updated = new Date(model.updated);
-            if(!err){
+            if (!err) {
                 return res.status(200).json({
                     item: rows.insertId,
                     detail: model
                 })
-            }else{
+            } else {
                 return res.status(500).json({
                     code: 500,
                     message: catchError(err.errno)
@@ -52,68 +52,68 @@ router.post('/', (req, res, next)=>{
 router.get("/", (req, res, next) => {
     var sp, lp, skip;
     if (!req.query["sp"] || !req.query["lp"]) {
-      sp = 0;
-      lp = 5;
+        sp = 0;
+        lp = 5;
     } else {
-      sp = Object.values(req.query["sp"]);
-      lp = Object.values(req.query["lp"]);
+        sp = Object.values(req.query["sp"]);
+        lp = Object.values(req.query["lp"]);
     }
-  
+
     skip = sp * lp;
-  
+
     var sql = "SELECT * FROM member LIMIT " + sp + "," + lp;
-  
+
     mysql_connection.query(sql, (err, rows, field) => {
-      if(!err){
-          return res.status(200).json({
-              total_items: rows.length,
-              items: rows,
+        if (!err) {
+            return res.status(200).json({
+                total_items: rows.length,
+                items: rows,
             });
-      }else{
-          return res.status(500).json({
-              code: 500,
-              text: err.name
-          })
-      }
+        } else {
+            return res.status(500).json({
+                code: 500,
+                text: err.name
+            })
+        }
     });
-  });
+});
 
 // R => Retrieve by ID
 router.get("/:_id", (req, res, next) => {
     let _id = req.params._id;
-  
+
     var sql = "SELECT * FROM member WHERE _id = " + _id;
-  
+
     mysql_connection.query(sql, (err, rows, field) => {
-      if(!err){
-          return res.status(200).json({
-              total_items: rows.length,
-              items: rows,
+        if (!err) {
+            return res.status(200).json({
+                total_items: rows.length,
+                items: rows,
             });
-      }else{
-          return res.status(500).json({
-              code: 500,
-              text: err.name
-          })
-      }
+        } else {
+            return res.status(500).json({
+                code: 500,
+                text: err.name
+            })
+        }
     });
-  });
+});
 
 // U = Update
-router.put('/', (req, res)=>{
+router.put('/', (req, res) => {
     let model = req.body;
     // var sql = "SET @EmpId = ?;SET @Name = ?; SET @EmpCode = ?; SET @Salary = ?; \
     // CALL EmployeeAddOrEdit(@EmpId,@Name,@EmpCode,@Salary)";
     model.updated = new Date().getTime();
     let sql = "UPDATE member SET name='" + model.name + "', username='" + model.username + "', password='" + model.password + "', role=" + model.role + ", status=" + model.status + ",company=" + model.company + ", updated=" + model.updated + "";
-    sql+= " WHERE _id = " + model._id;
+    sql += " WHERE _id = " + model._id;
 
-    mysql_connection.query(sql,(err,rows,fields)=>{
-        if(!err){
+    mysql_connection.query(sql, (err, rows, fields) => {
+        if (!err) {
             return res.status(200).json({
                 item: model
             })
-        }else{
+        } else {
             return res.status(500).json({
                 code: 500,
                 message: catchError(err.errno)
@@ -123,17 +123,17 @@ router.put('/', (req, res)=>{
 })
 
 // D = Delete
-router.delete('/:_id', (req, res)=>{
+router.delete('/:_id', (req, res) => {
     let _id = req.params._id;
 
     var sql = "DELETE FROM member WHERE _id = " + _id
-    mysql_connection.query(sql,(err,rows,fields)=>{
-        if(!err){
+    mysql_connection.query(sql, (err, rows, fields) => {
+        if (!err) {
             return res.status(200).json({
                 message: "ลบข้อมูลพนักงานสำเร็จ",
                 affected: "ส่งผลกระทบกับ " + rows.affectedRows + " เรคคอร์ด"
             })
-        }else{
+        } else {
             return res.status(500).json({
                 code: 500,
                 message: catchError(err.errno)
@@ -143,30 +143,35 @@ router.delete('/:_id', (req, res)=>{
 })
 
 // Login
-router.post("/login", (req, res, next)=>{
+router.post("/login", (req, res, next) => {
     let model = req.body;
-    sql="SELECT _id, username, password, name, role FROM member WHERE username ='" + model.username + "'";
+    sql = "SELECT _id, username, password, name, role FROM member WHERE username ='" + model.username + "'";
     mysql_connection.query(sql, (err, rows, field) => {
-        bcrypt.compare(model.password, rows[0].password, (err,result)=>{
-            if(err) {
+        bcrypt.compare(model.password, rows[0].password, (err, result) => {
+            if (err) {
                 return res.status(500).json({
                     code: 500,
                     message: catchError(err.errno)
                 })
             }
 
-            sql="UPDATE member SET updated = " + new Date().getTime();
-            mysql_connection.query(sql, (err, sub_rows, field)=>{
-                if(!err){
+            sql = "UPDATE member SET updated = " + new Date().getTime();
+            mysql_connection.query(sql, (err, sub_rows, field) => {
+                if (!err) {
                     const token = jwt.sign({
                         username: user[0].username,
-                        userId: user[0]._id
-                    })
+                        userId: user[0]._id,
+                      },
+                      "secret", {
+                        expiresIn: "24h",
+                      }
+                    );
+                    accessToken = token;
 
                     return res.status(200).json({
                         code: 200,
                         message: "Auth Successful",
-                        user : rows,
+                        user: rows[0],
                         accessToken: token
                     })
                 }
@@ -176,17 +181,17 @@ router.post("/login", (req, res, next)=>{
                 })
             })
         })
-        if(err){
+        if (err) {
             return res.status(500).json({
                 code: 500,
                 message: catchError(err.errno)
             })
         }
-      });
+    });
 })
 
-function catchError(code){
-    if(code=="1062"){
+function catchError(code) {
+    if (code == "1062") {
         return "กรุณาเปลี่ยน Username ใหม่ !"
     }
 }
