2496e965843a8bcce343e2ced4e32d2e071af367
tct48 <tar_solo@outlook.co.th>
2020-09-08 19:28:20 +0700

    push to server

--- api/routes/receipt.js
+++ api/routes/receipt.js
@@ -10,7 +10,7 @@ var mysql_connection = mysql.createConnection({
     database: "deejung1_pos",
     multipleStatements: true,
 });
-mysql_connection.timeout=0;
+mysql_connection.timeout = 0;
 
 // C => Created Receipt
 router.post('/', (req, res, next) => {
@@ -23,11 +23,11 @@ router.post('/', (req, res, next) => {
     mysql_connection.query(sql, (err, rows, fields) => {
         model.receipt_no = 1;
         if (rows.receipt_no) {
-            model.receipt_no = rows.receipt_no+1;
+            model.receipt_no = rows.receipt_no + 1;
             console.log(rows);
         }
         var sql = "INSERT INTO receipt (receipt_no, customer, title, address, type, total, company, member, created, updated, status, view) VALUES (";
-        sql += "" + model.receipt_no + ", '" + model.customer + "', '"  + model.title + "', '" + model.address + "', " + model.type + ", " + model.total + ", " + model.company + ", " + model.member + ", " + model.created + ", " + model.updated + ", 1,1)";
+        sql += "" + model.receipt_no + ", '" + model.customer + "', '" + model.title + "', '" + model.address + "', " + model.type + ", " + model.total + ", " + model.company + ", " + model.member + ", " + model.created + ", " + model.updated + ", 1,1)";
         console.log(sql)
         mysql_connection.query(sql, (err, rows, fields) => {
             if (!err) {
@@ -91,7 +91,7 @@ router.get("/", (req, res, next) => {
 
     let sql = "SELECT * FROM receipt";
 
-    if(req.query["company"]!="" && req.query["role"]!=""){
+    if (req.query["company"] != "" && req.query["role"] != "") {
         company = Object.values(req.query["company"])[0];
         role = Object.values(req.query["role"])[0];
         console.log(company)
@@ -99,28 +99,28 @@ router.get("/", (req, res, next) => {
         // 5 == all
         sql += " WHERE company=" + company + " AND view=1";
     }
-    
+
     skip = sp * lp;
 
-    
-    
+
+
     console.log(sql);
 
     mysql_connection.query(sql, (err, rows, field) => {
         if (!err) {
-            sql+=" ORDER BY _id DESC";
-            sql+=" LIMIT " + skip + ", " + lp;
-            mysql_connection.query(sql, (err, sub_rows, field)=>{
-                if(!err){
+            sql += " ORDER BY _id DESC";
+            sql += " LIMIT " + skip + ", " + lp;
+            mysql_connection.query(sql, (err, sub_rows, field) => {
+                if (!err) {
                     return res.status(200).json({
                         total_items: rows.length,
                         items: sub_rows,
                     });
-                }else{
+                } else {
 
                 }
             })
-            
+
         } else {
             console.log("********* ERROR ************");
             console.log(err)
@@ -128,9 +128,45 @@ router.get("/", (req, res, next) => {
                 code: 500,
                 message: catchError(err.errno)
             })
-        } 
+        }
     });
 });
+
+router.get("/daily", (req, res, next) => {
+    let current_date = new Date();
+    let date = current_date.getDate();
+    let month = current_date.getMonth() + 1;
+    let year = current_date.getFullYear();
+    var round;
+    // 1095 1096
+    var sql = "SELECT receipt._id, dor, title, category.name AS type, total FROM receipt, category WHERE category._id = receipt.type AND DAY(dor)=" + date + " AND MONTH(dor)=" + month + " AND YEAR(dor)=" + year + " AND company=1 ORDER BY receipt._id";
+    mysql_connection.query(sql, (err, items, field) => {
+        if (!err) {
+            round = items.length;
+            return res.status(200).json({
+                total_items: round,
+                items: items
+            })
+        }
+    })
+})
+
+router.get("/dailyDetail/:_id", (req, res, next) => {
+    let _id = req.params._id; 
+    var sql = "SELECT * FROM receipt_detail WHERE receipt=" + _id;
+    mysql_connection.query(sql, (err, items, field) => {
+        if (!err) {
+            return res.status(200).json({
+                items: items
+            })
+        }else{
+            return res.status(500).json({
+                code: 500,
+                message: catchError(err.errno)
+            })
+        }
+    })
+})
 
 // R => Retrieve All Receipt From Filter
 router.get("/search", (req, res, next) => {
@@ -145,63 +181,63 @@ router.get("/search", (req, res, next) => {
 
     let sql = "SELECT * FROM receipt";
 
-    if(req.query["company"]!="" && req.query["role"]!=""){
+    if (req.query["company"] != "" && req.query["role"] != "") {
         company = Object.values(req.query["company"])[0];
         role = Object.values(req.query["role"])[0];
 
-        if(Object.values(req.query["textSearch"])[0]!="undefined"){
+        if (Object.values(req.query["textSearch"])[0] != "undefined") {
             filter = req.query["textSearch"]
-        }else
-            filter="";
-        
+        } else
+            filter = "";
+
         console.log(req.query["textSearch"]);
         // 5 == all
-        if(company==5 && role==1){
+        if (company == 5 && role == 1) {
             sql += " WHERE customer LIKE '%" + filter + "%' OR title LIKE '%" + filter + "%'";
-        }else{
+        } else {
             sql += " WHERE company=" + company + " AND (customer LIKE '%" + filter + "%' OR title LIKE '%" + filter + "%' OR _id LIKE '%" + filter + "%')";
         }
     }
-    
+
     skip = sp * lp;
     console.log(sql)
 
     mysql_connection.query(sql, (err, rows, field) => {
         if (!err) {
-            sql+=" ORDER BY _id DESC";
-            sql+=" LIMIT " + skip + ", " + lp;
-            mysql_connection.query(sql, (err,sub_rows, field)=> {
-                if(!err){
+            sql += " ORDER BY _id DESC";
+            sql += " LIMIT " + skip + ", " + lp;
+            mysql_connection.query(sql, (err, sub_rows, field) => {
+                if (!err) {
                     return res.status(200).json({
                         total_items: rows.length,
                         items: sub_rows,
                     });
-                }else{
+                } else {
                     return res.status(500).json({
                         code: 500,
                         message: catchError(err.errno)
                     })
                 }
             })
-            
+
         } else {
             return res.status(500).json({
                 code: 500,
                 message: catchError(err.errno)
             })
-        } 
+        }
     });
 });
 
-router.get("/trash", (req, res, next)=>{
-    let sql="SELECT receipt._id,title, receipt_no,dor, customer,category.name AS type, company.name AS company,member, receipt.status, receipt.updated , member.name, total FROM receipt, member, company, category WHERE view=0 AND member._id = receipt.deleted_by AND company._id = receipt.company AND category._id = receipt.type"
-    mysql_connection.query(sql, (err, rows, field)=>{
-        if(!err){
+router.get("/trash", (req, res, next) => {
+    let sql = "SELECT receipt._id,title, receipt_no,dor, customer,category.name AS type, company.name AS company,member, receipt.status, receipt.updated , member.name, total FROM receipt, member, company, category WHERE view=0 AND member._id = receipt.deleted_by AND company._id = receipt.company AND category._id = receipt.type"
+    mysql_connection.query(sql, (err, rows, field) => {
+        if (!err) {
             return res.status(200).json({
-                total_items:rows.length,
+                total_items: rows.length,
                 items: rows
             })
-        }else{
+        } else {
             return res.status(500).json({
                 code: 500,
                 message: catchError(err.errno)
@@ -266,15 +302,15 @@ router.put('/', (req, res) => {
 })
 
 // U = Update Status Bill by ID
-router.put('/status',(req, res)=>{
+router.put('/status', (req, res) => {
     let model = req.body;
     let sql = "UPDATE receipt SET status=" + model.status + " WHERE _id=" + model._id;
-    mysql_connection.query(sql, (err, rows,field) => {
-        if(!err){
+    mysql_connection.query(sql, (err, rows, field) => {
+        if (!err) {
             return res.status(200).json({
-                receipt:rows
+                receipt: rows
             })
-        }else{
+        } else {
             return res.status(500).json({
                 code: 500,
                 message: catchError(err.err)
@@ -329,19 +365,19 @@ router.patch('/:_id', (req, res) => {
     })
 })
 
-router.delete('/:_id', (req, res)=>{
+router.delete('/:_id', (req, res) => {
     let _id = req.params._id;
     let sql = "DELETE FROM receipt WHERE _id = " + _id;
-    mysql_connection.query(sql, (err, rows, field)=>{
+    mysql_connection.query(sql, (err, rows, field) => {
         if (!err) {
             sql = "DELETE FROM receipt_detail WHERE receipt = " + _id;
-            mysql_connection.query(sql, (err, rows, field)=> {
-                if(!err){
+            mysql_connection.query(sql, (err, rows, field) => {
+                if (!err) {
                     return res.status(200).json({
                         message: "ลบข้อมูลบริษัทสำเร็จ",
                         affected: "ส่งผลกระทบกับ " + rows.affectedRows + " เรคคอร์ด"
                     })
-                }else{
+                } else {
                     return res.status(500).json({
                         code: 500,
                         message: catchError(err.errno)
