9143ac0b0f98fad2540cf8ecccffe85f98d2d74c
tct48 <tar_solo@outlook.co.th>
2020-09-11 15:27:10 +0700

    push to server fucking 15 to 40 limit summary

--- api/routes/summary.js
+++ api/routes/summary.js
@@ -66,7 +66,8 @@ router.get("/:company", (req, res, next) => {
     // SELECT dor, SUM(total) as total_price FROM receipt WHERE company=1 GROUP BY MONTH(dor) ORDER BY MONTH(dor) DESC LIMIT 7
     let dumb = new Date();
     let company = req.params.company;
-    sql = "SELECT _id, dor, SUM(total) as total_price, status FROM receipt WHERE MONTH(dor)=" + (Number(dumb.getMonth())+1) + " AND view=1 AND company=" + company + " GROUP BY DAY(dor), status LIMIT 15";
+    sql = "SELECT _id, dor, SUM(total) as total_price, status FROM receipt WHERE MONTH(dor)=" + (Number(dumb.getMonth())+1) + " AND view=1 AND company=" + company + " GROUP BY DAY(dor), status LIMIT 40";
+    console.log(sql);
     mysql_connection.query(sql, (err, rows, field) => {
             if (!err) {
                 return res.status(200).json({
